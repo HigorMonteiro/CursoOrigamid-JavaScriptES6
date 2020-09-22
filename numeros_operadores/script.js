7a2f76284d6da2b54d164006a7d34ba461e9687c
Higor Vinicius <higor.tecinfor@gmail.com>
2020-09-21 21:56:21 -0300

    examples numbers

---
+++ numeros_operadores/script.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+
+// Crie duas expressões que retornem NaN
+var number = '10e'*10;
+var number2 = '35%' / 2
+
+// Soma a strig '200' com o número 50 e retornar 250
+var calculate = +'200'+50
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 7
+console.log(++x)
+
+// Como dividir o peso por 2?
+var weight = +'80' / 2;
+var unity = 'kg'
+var total_weight = weight + unity
+var result = total_weight 
+
+console.log(result)
