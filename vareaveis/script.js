f9460695481f19a321d8a907bc4abb3d7e7a836b
Higor Vinicius <higor.tecinfor@gmail.com>
2020-09-21 21:34:47 -0300

    examples used variables

---
+++ vareaveis/script.js
@@ -0,0 +1,13 @@
+// Declarar uma variável com o seu nome
+var name = "Higor Vinicius";
+// Declarar uma variável com a sua idade
+var age = 27;
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var favoriteFood;
+// Atribuir valor a sua comida favorita
+var favoriteFood = "Creme de galinha"
+// Declarar 5 variáveis diferentes sem valores
+
+var test1, test2, test3, test4, test5;
+
+console.log(name, age, favoriteFood)
