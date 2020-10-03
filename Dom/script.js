89a83070c4c7233a296a9197f7fcf91cbee61500
Higor Vinicius <higor.tecinfor@gmail.com>
2020-10-03 17:26:47 -0300

    practical activity

--- Dom/script.js
+++ Dom/script.js
@@ -1,19 +1,32 @@
-const href = window.location.href;
+// const href = window.location.href;
 
-console.log(href)
+// console.log(href)
 
-if(href === 'http://127.0.0.1:5500/Dom/index.html'){
-  console.log('É igual')
-}
+// if(href === 'http://127.0.0.1:5500/Dom/index.html'){
+//   console.log('É igual')
+// }
 
-const titulo = document.querySelector('h1')
+// const titulo = document.querySelector('h1')
 
-console.log(titulo)
+// console.log(titulo)
 
-const idTitulo = titulo.id;
-const textElemento = titulo.innerText;
-const auturaElemento = titulo.offsetHeight;
+// const idTitulo = titulo.id;
+// const textElemento = titulo.innerText;
+// const auturaElemento = titulo.offsetHeight;
 
-titulo.addEventListener('click', function(){
-  console.log('Clicou no titulo : ', titulo.innerText)
-});
+// titulo.addEventListener('click', function(){
+//   console.log('Clicou no titulo : ', titulo.innerText)
+// });
+
+// Retorne a URL da pagina atual ultizando o objecto window
+
+const href = window.location.href
+console.log('URL da pagina atual : ', href)
+
+// Selecione o primero elemento da pagina que possua a classe ativo
+console.log(document.querySelector('.ativo'))
+
+// Retorna a linguagem do navegador
+console.log(window.navigator.language)
+// Retorne a largura da janela
+console.log(window.window.innerWidth)
